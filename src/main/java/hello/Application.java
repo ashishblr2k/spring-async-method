57c624f3dcd8a0f3a569fc6842b452dad5386fce
ashishblr2k <ashishblr2k@gmail.com>
2017-04-21 17:04:14 +0530

    first commit

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,33 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.AsyncConfigurerSupport;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+/**
+ * Created by agup101 on 4/21/2017.
+ */
+@SpringBootApplication
+@EnableAsync
+public class Application extends AsyncConfigurerSupport {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(2);
+        executor.setMaxPoolSize(2);
+        executor.setQueueCapacity(500);
+        executor.setThreadNamePrefix("GithubLookup-");
+        executor.initialize();
+        return executor;
+    }
+
+}
